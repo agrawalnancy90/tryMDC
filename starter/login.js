8b6e494a8cb4a4f66fa5ba6b3c4013e31cf1157e
Nancy Agrawal <nagraw5@uic.edu>
2019-04-07 19:16:41 -0500

    UX look and feel complete

--- starter/login.js
+++ starter/login.js
@@ -1,8 +1,3 @@
-import {MDCRipple} from '@material/ripple';
-import {MDCTextField} from '@material/textfield';
+import {MDCNotchedOutline} from '@material/notched-outline';
 
-const username = new MDCTextField(document.querySelector('.username'));
-const password = new MDCTextField(document.querySelector('.password'));
-
-new MDCRipple(document.querySelector('.cancel'));
-new MDCRipple(document.querySelector('.next'));
+const notchedOutline = new MDCNotchedOutline(document.querySelector('.mdc-notched-outline'));
